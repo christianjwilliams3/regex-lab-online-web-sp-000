7ef5e80f25164f3b1c065ee79904974f41943286
christian williams <christian.williamsj@gmail.com>
2019-09-03 06:59:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
-def starts_with_a_vowel?([aeiou]).to be(true)
-  
+def starts_with_a_vowel?(word).to be(true)
+  if = nil 
+    return = false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
