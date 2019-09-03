f8c372de2b37e15b93a3a0cd777a21d43de9f5e4
christian williams <christian.williamsj@gmail.com>
2019-09-03 07:36:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].*\.$/)
+  text.match(/^[A-Z].*\.$/) == nil
+  
 end
 
 def valid_phone_number?(phone)
