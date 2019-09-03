699a09d96b09ac71c7ee9acbc60382642982f84a
christian williams <christian.williamsj@gmail.com>
2019-09-03 07:38:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,8 +15,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].*\.$/) == nil
-  return false 
+  if text.match(/^[A-Z].*\.$/) == nil
+   return false 
 else
   return true 
 end
